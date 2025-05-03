35a7710f3bd9b48d3162c5ddf723a23e6d1cad75
David Knecht <david.knecht@ic-consult.de>
2025-05-03 12:37:13 +0200

    Extract PageFooter to make it reusable

---
+++ PageFooter.js
@@ -0,0 +1,10 @@
+
+export default {
+  setup() {},
+  template: `
+    <footer class=" my-3 text-center">
+      This Tool is based on Grimwild © 2024 by J.D. Maxwell and Oddity Press, licensed under CC BY 4.0.
+    </footer>`
+  // Can also target an in-DOM template:
+  // template: '#my-template-element'
+}
